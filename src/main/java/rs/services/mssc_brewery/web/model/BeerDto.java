06c18ba81248657ce4d0424b34ee84c7add646a5
goran <goran.djermanovic@neomatic.io>
2025-01-12 10:49:33 +0100

    pocetno

---
+++ src/main/java/rs/services/mssc_brewery/web/model/BeerDto.java
@@ -0,0 +1,5 @@
+package rs.services.mssc_brewery.web.model;
+
+public class BeerDto {
+
+}
---
+++ src/test/java/rs/services/mssc_brewery/MsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.services.mssc_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
