3837086afeeb3b41f3aed30a5476a9c3b8eec65c
goran <goran.djermanovic@neomatic.io>
2025-02-05 19:16:08 +0100

    bean validations

--- src/main/java/rs/services/mssc_brewery/web/controller/CustomerController.java
+++ src/main/java/rs/services/mssc_brewery/web/controller/CustomerController.java
@@ -1,14 +1,11 @@
 package rs.services.mssc_brewery.web.controller;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import rs.services.mssc_brewery.services.CustomerService;
 import rs.services.mssc_brewery.web.model.CustomerDto;
--- src/main/java/rs/services/mssc_brewery/web/controller/v2/BeerControllerV2.java
+++ src/main/java/rs/services/mssc_brewery/web/controller/v2/BeerControllerV2.java
@@ -1,7 +1,5 @@
 package rs.services.mssc_brewery.web.controller.v2;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.UUID;
 
 import org.springframework.http.HttpHeaders;
@@ -9,7 +7,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +16,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.ConstraintViolationException;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import rs.services.mssc_brewery.services.v2.BeerServiceV2;
